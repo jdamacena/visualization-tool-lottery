e2dbfa31c6bd74545a7b27d32ba3546090bcc146
Junior Damacena <19500428+jdamacena@users.noreply.github.com>
2025-09-01 01:11:46 -0300

    Esta é uma ferramenta de visualização simples que ajuda a entender melhor as chances de alguém ganhar na loteria.

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,82 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+@file:Suppress("SpellCheckingInspection")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+import java.text.NumberFormat
+import java.util.Locale
+
+fun Double.formatarDinheiro(locale: Locale = Locale.of("pt", "BR")): String {
+    val nf = NumberFormat.getCurrencyInstance(locale)
+    return nf.format(this)
 }
+
+fun Long.formatarDinheiro(locale: Locale = Locale.of("pt", "BR")): String {
+    val nf = NumberFormat.getCurrencyInstance(locale)
+    return nf.format(this)
+}
+
+fun Int.formatarQuantidade(locale: Locale = Locale.of("pt", "BR")): String {
+    val nf = NumberFormat.getNumberInstance(locale)
+    return nf.format(this)
+}
+
+fun main() {
+    val chancesDeGanhar = 50_000_000 // no formato 1 chance em [variável] ex. 1 em 500
+    val precoApostaEmReais = 5.50
+    val valorPremioEmReais = 50_000_000.00
+    val numSimulacoes = 10
+
+    val totalApostasFeitas = mutableListOf<Int>()
+
+    (1..numSimulacoes).forEach { _ ->
+        var qtdApostas = 0
+        var ganhouNaLoteria = false
+
+        while (!ganhouNaLoteria) {
+            qtdApostas++
+
+            val totalGastoEmApostas = qtdApostas * precoApostaEmReais
+
+            val numeroAposta = (1..chancesDeGanhar).random()
+            val numeroSorteio = (1..chancesDeGanhar).random()
+
+            if (numeroSorteio == numeroAposta) {
+                ganhouNaLoteria = true
+
+                val lucro: Double = valorPremioEmReais - totalGastoEmApostas
+
+                println("\n".padEnd(50, '-'))
+                println("Parabéns, você ganhou na loteria com ${qtdApostas.formatarQuantidade()} apostas")
+                println("\tVocê gastou: ${totalGastoEmApostas.formatarDinheiro()}")
+                println("\tVocê ganhou: ${valorPremioEmReais.formatarDinheiro()}")
+                println("\tLucro:       ${lucro.formatarDinheiro()}")
+                println("\n".padStart(50, '-'))
+            } else {
+                if (qtdApostas % 1_000_000 == 0)
+                    println("Aposta #${qtdApostas.formatarQuantidade()} - Total gasto: ${totalGastoEmApostas.formatarDinheiro()}")
+            }
+        }
+        totalApostasFeitas.add(qtdApostas)
+    }
+
+    val totalGanhos = numSimulacoes * valorPremioEmReais
+    val totalGastos: Long = (totalApostasFeitas.sum() * precoApostaEmReais).toLong()
+    val totalLucro = totalGanhos - totalGastos
+
+    println("\n\nDepois de ${numSimulacoes.formatarQuantidade()} simulações e ${totalApostasFeitas.sum().formatarQuantidade()} apostas")
+    println("\tVocê ganhou:     ${totalGanhos.formatarDinheiro()}")
+    println("\tVocê perdeu:     ${totalGastos.formatarDinheiro()}")
+    println("\tTotal de ${if (totalLucro  > 0) "lucros" else "perdas"}: ${totalLucro.formatarDinheiro()}")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
